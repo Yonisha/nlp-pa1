79606dd6a5211079f2f2035acc817797aa5a9e8f
morshka <mor.sheffer@gmail.com>
2015-03-28 09:57:41 +0300

    Added missing file

---
+++ src/main/java/NgramWithProb.java
@@ -0,0 +1,14 @@
+public class NgramWithProb{
+    private String ngram;
+    private double prob;
+
+    public NgramWithProb(String ngram, double prob){
+        this.ngram = ngram;
+        this.prob = prob;
+    }
+
+    @Override
+    public String toString(){
+        return  prob + "\t" + ngram + "\r\n";
+    }
+}
